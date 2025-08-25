b3c9660e988cc279c4344289315bdf3d5bcf52b0
Pratik Chaubey <pratikup98@gmail.com>
2025-08-26 00:29:42 +0530

    Fix: corrected image path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import HomePage from './Components/HomePage';
-import bg from './assets/bg.png'; // Correct relative path from src/App.jsx
+import Background from './assets/Background.png'
 function App() {
   return (
     <div>
     <div
       className="h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bg})` }}
+      style={{ backgroundImage: `url(${Background})` }}
     >
       
       
